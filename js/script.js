eb194e80910c9e7efdb67435c38b53b3958a3487
Gomolemobig15 <gphenyo57@gmail.com>
2025-08-27 02:11:04 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+// Simple beginner-style JS
+// Shows an alert when the enquiry form is submitted.
+function handleSubmit(e){
+  e.preventDefault();
+  alert('Thanks! Your enquiry has been received.');
+}
